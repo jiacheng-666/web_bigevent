73123fcc85b644f7ec3ca7e246da84c6c98e9b02
jiacheng-666 <984542592@qq.com>
2021-08-20 18:09:03 +0800

    完成个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,41 @@
-$(function(){
+$(function () {
     // 调用getUserInfo 获取用户基本信息
     getUserInfo()
 
-    var layer =layui.layer
-    $('#btnLogout').on('click',function(){
+    var layer = layui.layer
+
+    //点击按钮，实现退出功能
+    $('#btnLogout').on('click', function () {
         // 提示用户是否确认退出
-        layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+        layer.confirm('确定退出登录?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
             //do something
             //1. 清空本的存储中的token
             localStorage.removeItem('token')
             //2. 重新跳转到登录页面
-            location.href='/login.html'
+            location.href = '/login.html'
 
             //关闭 comfirm 询问框
             layer.close(index);
-          });
+        });
     })
 
 })
 
 //获取用户的基本信息
-function getUserInfo(){
+function getUserInfo() {
     $.ajax({
-        method:'GET',
-        url:'/my/userinfo',
+        method: 'GET',
+        url: '/my/userinfo',
         //请求头配置对象
         // headers:{
         //     Authorization: localStorage.getItem('token') ||''
         // },
-        success: function(res){
+        success: function (res) {
             // console.log(res);
-            if(res.status !== 0) {
+            if (res.status !== 0) {
                 return layui.layer.msg(res.message)
             }
             //调用renderAvatar 渲染用户的头像
@@ -51,22 +56,22 @@ function getUserInfo(){
 }
 
 //渲染用户头像
-function renderAvatar(user){
+function renderAvatar(user) {
     //1.获取用户的名称
-    var name =user.nickname || user.username
+    var name = user.nickname || user.username
     //2.设置欢迎的文本
-    $('#welcome').html('欢迎&nbsp;&nbsp;'+name)
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
     //3.按需渲染用户的头像
-    if(user.user_pic!== null){
+    if (user.user_pic !== null) {
         //3.1渲染图片头像
-        $('.layui-nav-img').attr('src',user_pic).show()
+        $('.layui-nav-img').attr('src',user.user_pic).show()
         $('.text-avatar').hide()
-    }else{
+    } else {
         //3.2渲染文本头像
         $('.layui-nav-img').hide()
-        var first =name[0].toUpperCase()
+        var first = name[0].toUpperCase()
         $('.text-avatar').html(first).show()
-        
+
 
     }
 }
---
+++ assets/js/user/user_info.js
@@ -0,0 +1,59 @@
+$(function () {
+    var form = layui.form
+    var layer = layui.layer
+
+    // 创建自己的验证规则
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return '昵称长度必须在1~6个字符之间！'
+            }
+        }
+    })
+
+    initUserInfo()
+
+    //初始化用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！')
+                }
+                // console.log(res);
+                //调用form.val()快速为标点赋值
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+
+    //重置表单的数据
+    $('#btnReset').on('click', function (e) {
+        //阻止表单的默认重置行为
+        e.preventDefault()
+        initUserInfo()
+    })
+
+    //监听表单的提交事件
+    $('.layui-form').on('submit', function (e) {
+        //阻止表单的默认提交行为
+        e.preventDefault()
+        //发起ajax数据请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败')
+                }
+                layer.msg('更新用户信息成功')
+                //调用父页面中的方法，重新渲染用户头像和用户的信息
+                //window=>iframe窗口  window.parent=> index页面
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
